935eeeaf5647f20a09f41aa688ddd226246967f0
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2024-11-09 08:24:42 +0100

    chore: add test script and correct workspace overrides
    : commit message

---
+++ packages/test/index.js
@@ -0,0 +1,33 @@
+import {
+  pipe,
+} from "sanctuary";
+
+// import {
+//   config
+// } from "sanctuary-def";
+
+
+
+
+// promise(resolve("moin")).then(console.log).catch(console.error);
+// console.log(StrMap(String));
+
+// const x = def("x")({})([
+//   String,
+//   FutureType(ErrorType)(ObjectType),
+//   // FutureType(ErrorType)(StrMap(String)),
+// ])(() => resolve(
+//   // [1] // error
+//   { x: "t" } // works
+//   // { x: "t", y: 1 } // error
+// ));
+
+// promise(x()).then(console.log).catch(console.error);
+
+// console.log("config", config.checkTypes)
+
+pipe([
+  // x,
+  // resolve,
+  // promise
+])("test").then(console.log).catch(console.error);
